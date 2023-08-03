3365dccf8c2b5a6fb15ba4c5b20d9af284d576ee
amirali079 <aligoli079@gmail.com>
2023-08-03 16:56:34 +0330

    init

---
+++ WriteExcel/CompressSample.cs
@@ -0,0 +1,17 @@
+﻿using ICSharpCode.SharpZipLib.BZip2;
+
+namespace ReadExcel;
+
+// use SharpZipLib 1.4.2
+public class CompressSample
+{
+    private static Stream Decompress(Stream readStream)
+    {
+        var memoryStream = new MemoryStream();
+
+        BZip2.Decompress(readStream, memoryStream, false);
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        return memoryStream;
+    }
+
+}
---
+++ WriteExcel/Program.cs
@@ -0,0 +1,9 @@
+﻿using NPOI.XSSF.Streaming;
+
+// use NPOI last version for write Excel
+
+string filePath = "npoiFileTest.xlsx";
+using var stream = new FileStream(filePath, FileMode.Create);
+
+var workbook = new SXSSFWorkbook(2000);
+workbook.Write(stream);
